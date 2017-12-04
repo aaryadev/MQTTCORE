4723d4abc3363b5b724af7e0b31388fdb177f3f0
aaryadev <devmgbhat@gmail.com>
2017-12-05 02:43:20 +0530

    Basics

---
+++ UI/static/action.js
@@ -0,0 +1,7 @@
+$( document ).ready(function(){
+	resize_view();
+	$('body').removeClass('blur_5')
+});
+
+
+
---
+++ server.js
@@ -0,0 +1,40 @@
+var static = require('node-static');
+var file = new static.Server('./UI');
+var mosca = require('mosca');
+//vars
+var deviceCount=0;
+var channels=[];
+
+var settings = {
+  port: rocess.env.PORT || 9999
+};
+
+
+//Define
+var server = require('http').createServer(function(request, response) {
+    request.addListener('end', function() {
+        file.serve(request, response);
+    }).resume();
+});
+
+server.listen(rocess.env.PORT || 9999);
+console.log("Server Started");
+var ms = new mosca.Server(settings);
+
+ms.on('clientConnected', function(client) {
+   console.log('client connected', client.id);
+});
+
+ms.on('published', function(packet, client) {
+  console.log('Published', packet.topic, packet.payload);
+});
+
+server.on('clientDisconnected', function(client) {
+console.log('Client Disconnected:', client.id);
+});
+ms.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running');
+}
